3da56da7dd4d2ae72508b4aa2e9bf869e485264d
NameeNomen <fat.lp3i33@gmail.com>
2026-01-12 15:02:40 +0700

    buat login

---
+++ lib/helper/loginHelper.dart
@@ -0,0 +1,45 @@
+import 'dart:convert';
+import 'package:http/http.dart' as xubin;
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import '/model/login.dart';
+
+class LoginHelper{
+  static const String myuri= 'https://thioacetic-unreticently-saul.ngrok-free.dev/yunisafa';
+  static final storage = FlutterSecureStorage();
+  final Map<String, String> _header = {
+    "Content-Type": "application/json",
+    "ngrok-skip-browser-warning":"true",
+  };
+
+  Future<Login?> login(String username, String passwordHash) async{
+    final uri = Uri.parse('$myuri/login');
+    try{
+      final bales = await xubin.post(
+        uri,
+        headers: _header,
+        body: jsonEncode({'Username':username, 'PasswordHash': passwordHash,})
+
+      );
+
+      if(bales.statusCode == 200){
+        final data = jsonDecode(bales.body);
+
+        await storage.write(key: 'token', value: data['token']);
+        return Login.fromJson(data);
+      }else{
+        print ("Hahaha gagal: ${bales.body}");
+        return null;
+      }
+    }catch (e){
+      print ('Error saat anda login please jangan maksa: $e');
+      return null;
+    }
+  }
+
+  Future<String?> getToken()async{
+    return await storage.read(key: 'token');
+  }
+  Future<void> logout() async {
+    await storage.delete(key: 'token');
+  }
+}
---
+++ lib/model/user.dart
@@ -0,0 +1,55 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+
+class UserAbsen {
+  final int id;
+  final String username;
+
+  const UserAbsen({
+    required this.id,
+    required this.username
+  });
+
+  UserAbsen copyWith({
+    int? id,
+    String? username,
+  }) {
+    return UserAbsen(
+      id: id ?? this.id,
+      username: username ?? this.username,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'id': id,
+      'username': username,
+    };
+  }
+
+  factory UserAbsen.fromMap(Map<String, dynamic> map) {
+    return UserAbsen(
+      id: (map["id"] ?? 0) as int,
+      username: (map["username"] ?? '') as String,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory UserAbsen.fromJson(String source) => UserAbsen.fromMap(json.decode(source) as Map<String, dynamic>);
+
+  @override
+  String toString() => 'UserAbsen(id: $id, username: $username)';
+
+  @override
+  bool operator ==(covariant UserAbsen other) {
+    if (identical(this, other)) return true;
+  
+    return 
+      other.id == id &&
+      other.username == username;
+  }
+
+  @override
+  int get hashCode => id.hashCode ^ username.hashCode;
+}
